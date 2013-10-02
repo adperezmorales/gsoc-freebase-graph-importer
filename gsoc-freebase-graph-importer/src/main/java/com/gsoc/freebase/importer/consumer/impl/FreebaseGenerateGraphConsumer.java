86382b28530758c46f72b47bb22fa89c12ebca84
akroston <adrian.roston@gmail.com>
2013-10-02 14:10:52 -0700

    try catch

--- gsoc-freebase-graph-importer/src/main/java/com/gsoc/freebase/importer/consumer/impl/FreebaseGenerateGraphConsumer.java
+++ gsoc-freebase-graph-importer/src/main/java/com/gsoc/freebase/importer/consumer/impl/FreebaseGenerateGraphConsumer.java
@@ -1,11 +1,5 @@
 package com.gsoc.freebase.importer.consumer.impl;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CountDownLatch;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.gsoc.freebase.importer.ImporterConstants;
 import com.gsoc.freebase.importer.consumer.FreebaseConsumer;
 import com.gsoc.freebase.importer.model.Entity;
@@ -14,6 +8,11 @@ import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.KeyIndexableGraph;
 import com.tinkerpop.blueprints.TransactionalGraph;
 import com.tinkerpop.blueprints.Vertex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * <p>
@@ -22,12 +21,10 @@ import com.tinkerpop.blueprints.Vertex;
  * <p>
  * It is responsible of create the graph structure (Vertices)
  * </p>
- * 
+ *
  * @author Antonio David Perez Morales <adperezmorales@gmail.com>
- * 
  */
-public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
-{
+public class FreebaseGenerateGraphConsumer implements FreebaseConsumer {
     /**
      * Logger
      */
@@ -52,13 +49,12 @@ public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
      * <p>
      * Constructs an instance of FreebaseGenerateGraphConsumer using the given latch, queue and graph
      * </p>
-     * 
+     *
      * @param startLatch the {@code CountDownLatch} object used to wait before start the process
-     * @param queue the {@code BlockingQueue<Entity>} used to consume entities
-     * @param graph the {@code Graph} instance used to store the vertices
+     * @param queue      the {@code BlockingQueue<Entity>} used to consume entities
+     * @param graph      the {@code Graph} instance used to store the vertices
      */
-    public FreebaseGenerateGraphConsumer(CountDownLatch startLatch, BlockingQueue<Entity> queue, Graph graph)
-    {
+    public FreebaseGenerateGraphConsumer(CountDownLatch startLatch, BlockingQueue<Entity> queue, Graph graph) {
         this.startLatch = startLatch;
         this.queue = queue;
         this.graph = graph;
@@ -75,15 +71,11 @@ public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
      * This index is used later to looking for vertices in a faster way (managed by the graph implementation)
      * </p>
      */
-    private void initGraph()
-    {
-        if (this.graph.getFeatures().supportsVertexKeyIndex)
-        {
+    private void initGraph() {
+        if (this.graph.getFeatures().supportsVertexKeyIndex) {
             KeyIndexableGraph keyIndexableGraph = (KeyIndexableGraph) this.graph;
-            if (!keyIndexableGraph.getIndexedKeys(Vertex.class).contains(ImporterConstants.VERTEX_ENTITY_URI_PROPERTY))
-            {
-                synchronized (this.graph)
-                {
+            if (!keyIndexableGraph.getIndexedKeys(Vertex.class).contains(ImporterConstants.VERTEX_ENTITY_URI_PROPERTY)) {
+                synchronized (this.graph) {
                     keyIndexableGraph.createKeyIndex(ImporterConstants.VERTEX_ENTITY_URI_PROPERTY, Vertex.class);
                     /* Commit the vertex index if necessary */
                     if (this.graph.getFeatures().supportsTransactions)
@@ -103,10 +95,8 @@ public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
      * <p>Run the consumer process</p>
      * <p>It generates a new Vertex for each consumed Entity</p>
      */
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             logger.debug("Waiting the start in " + Thread.currentThread().getName());
             /*
              * Wait in the latch until other process tells this process to start
@@ -120,14 +110,12 @@ public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
             /*
              * Infinite loop to consume entities when produced until a certain Entity comes.
              */
-            while (true)
-            {
+            while (true) {
                 Entity entity = queue.poll();
                 if (entity == null)
                     continue;
 
-                if (entity.getUri().equals(ImporterConstants.CANCEL_ENTITY_URI))
-                {
+                if (entity.getUri().equals(ImporterConstants.CANCEL_ENTITY_URI)) {
                     // Adding the entity to stop the process. Other consumers will consume this entity to stop their
                     // process
                     queue.put(entity);
@@ -139,8 +127,7 @@ public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
                 /*
                  * Committing if the graph is transactional
                  */
-                if (processed++ % 2000 == 0)
-                {
+                if (processed++ % 2000 == 0) {
                     if (this.graph.getFeatures().supportsTransactions)
 
                     {
@@ -161,9 +148,7 @@ public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
             }
 
             logger.debug("Finishing " + Thread.currentThread().getName());
-        }
-        catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
@@ -174,39 +159,37 @@ public class FreebaseGenerateGraphConsumer implements FreebaseConsumer
      * <p>
      * Generates a new vertex in the graph for the current entity
      * </p>
-     * 
+     *
      * @param entity the {@code Entity} object to be used to generate the vertex
      */
-    private void generateVertex(Entity entity)
-    {
-        if (entity != null && entity.getProperties() != null)
-        {
-            // System.out.println("Processing entity: "+entity.getUri()+" in consumer "+Thread.currentThread().getName());
-            if (FreebaseUtils.isTopic(entity.getProperties()))
-            {
-                synchronized (this.graph)
-                {
-                    Vertex vertex = this.graph.addVertex(null);
-                    vertex.setProperty(ImporterConstants.VERTEX_ENTITY_URI_PROPERTY, entity.getUri());
+    private void generateVertex(Entity entity) {
+        try {
+            if (entity != null && entity.getProperties() != null) {
+                // System.out.println("Processing entity: "+entity.getUri()+" in consumer "+Thread.currentThread().getName());
+                if (FreebaseUtils.isTopic(entity.getProperties())) {
+                    synchronized (this.graph) {
+                        Vertex vertex = this.graph.addVertex(null);
+                        vertex.setProperty(ImporterConstants.VERTEX_ENTITY_URI_PROPERTY, entity.getUri());
 
                     /*
                      * Adding only rdf-type and type.object.name properties
                      */
-                    for (String property : entity.getProperties().keySet())
-                    {
-                        if (property.equals(ImporterConstants.RDF_TYPE))
-                        {
-                            vertex.setProperty(property, entity.getProperties().get(property));
-                        }
-                        if(property.equals(ImporterConstants.FREEBASE_TYPE_OBJECT_NAME)) {
+                        for (String property : entity.getProperties().keySet()) {
+                            if (property.equals(ImporterConstants.RDF_TYPE)) {
+                                vertex.setProperty(property, entity.getProperties().get(property));
+                            }
+                            if (property.equals(ImporterConstants.FREEBASE_TYPE_OBJECT_NAME)) {
 
-                            vertex.setProperty("name", entity.getProperties().get(property).get(0));
+                                vertex.setProperty("name", entity.getProperties().get(property).get(0));
+                            }
                         }
+
+                        vertex = null;
                     }
-
-                    vertex = null;
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
